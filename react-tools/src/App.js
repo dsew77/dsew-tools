ee6827ac5ace9c1bc89ca159b77c1a7a247f6154
dsew77 <rschampextreme@gmail.com>
2023-05-17 15:37:48 +1200

    Added Routers

--- react-tools/src/App.js
+++ react-tools/src/App.js
@@ -2,41 +2,31 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import Home from './Home'
-import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import Pomodoro from "./Pomodoro";
+
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
 
 function App() {
   return (
-    /*<div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>*/
+
       <Router>
           <div>
               <nav>
-                  <ul>
+                  <ul className="Top Bar">
                       <li>
                          <Link to="/">Home</Link>
+                      </li>
+                      <li>
+                        <Link to ="/Pomodoro">Pomodoro</Link>
                       </li>
                   </ul>
               </nav>
 
-              <Switch>
-                  <Route path="/">
-                      <Home />
-                  </Route>
-              </Switch>
+              <Routes>
+                  <Route path="/" element={<Home/>}/>
+                  <Route path="/Pomodoro" element={<Pomodoro/>}/>
+
+              </Routes>
           </div>
       </Router>
   );
---
+++ react-tools/src/Pomodoro.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Pomodoro(){
+    return(
+        <div>
+            <h2>Home</h2>
+            <p>Welcome to the Pomodoro!</p>
+        </div>
+    );
+}
+
+export default Pomodoro;
