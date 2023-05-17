b67d0c8bbe4462cd7c65164d884a41e5592d5d96
dsew77 <rschampextreme@gmail.com>
2023-05-17 15:24:04 +1200

    Added Router with Home page

--- react-tools/src/App.js
+++ react-tools/src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Home from './Home'
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
+    /*<div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,7 +21,24 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+    </div>*/
+      <Router>
+          <div>
+              <nav>
+                  <ul>
+                      <li>
+                         <Link to="/">Home</Link>
+                      </li>
+                  </ul>
+              </nav>
+
+              <Switch>
+                  <Route path="/">
+                      <Home />
+                  </Route>
+              </Switch>
+          </div>
+      </Router>
   );
 }
 
